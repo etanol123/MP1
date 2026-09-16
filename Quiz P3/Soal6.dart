2fd0c23c94170d2ccd6cf25a246f53aeddfb18d6
etanol123 <khoerulfadli.lp3i.krw25@gmail.com>
2026-09-16 19:57:37 +0700

    Quiz1pt2

---
+++ Quiz P3/Soal6.dart
@@ -0,0 +1,13 @@
+void tampilkanMenu({required String nama, required int harga, required bool tersedia}) {
+  String statusTersedia = tersedia ? "Ya" : "Tidak";
+  
+  print("=== DETAIL MENU ===");
+  print("Nama     : $nama");
+  print("Harga    : Rp$harga");
+  print("Tersedia : $statusTersedia");
+}
+
+void main() {
+  // Pemanggilan function dengan named parameter
+  tampilkanMenu(nama: "Ayam Geprek", harga: 18000, tersedia: true);
+}
